03f54efa5cb05b19f30009a4e44f87646a8228b3
William <williamsyoteki@gmail.com>
2023-12-07 20:34:11 +0900

    first commit

---
+++ src/main/java/com/william/kafka_quickstart/KafkaQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.william.kafka_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaQuickstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/william/kafka_quickstart/KafkaQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.kafka_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
